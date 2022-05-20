0904cc01a3ac62c4461b404f3af27b85c1c06c29
Justin Cooper <jcooper@chicousd.org>
2022-05-20 11:55:33 -0700

    pre mirror change

--- Backup-Shares.ps1
+++ Backup-Shares.ps1
@@ -1,6 +1,6 @@
-	###################################################################
-	# Run an incremental or full backup to a specified server/device. #
-	###################################################################
+###################################################################
+# Run an incremental or full backup to a specified server/device. #
+###################################################################
 # A text file is used to determine source and destination. 
 # Format the "JobFile" as such: srcServer|srcShare|dstServer|dstShare|excludeDirs|excludeFiles
 # The -JobFile Param is used to determine the JobFile. If the -Full is included a full copy is made.
@@ -12,37 +12,33 @@
 # The last line is to pass an object to an outside script for notification purposes.
 
 Param( 
-	[Parameter(Mandatory=$True)][ValidateNotNull()]$jobFile,
+	[Parameter(Mandatory = $True)][ValidateNotNull()]$jobFile,
 	[Switch]$Full,
 	[Switch]$Slow,
 	[Switch]$Test
 ) 
 # CLS
 
-if ($Full )
-{ 
+if ($Full ) { 
 	"Full Backup"
 	$scope = "Full"
-	$options = @("/MIR") 
+	$options = @("/E", "/M")
 }
-else 
-{
+else {
 	"Daily Backup"
 	$scope = $(Date).DayofWeek
-	$options = @("/S", "/M") 
+	$options = @("/E", "/M") 
 }
 
-if ($Test) 
-{
+if ($Test) {
 	"Test Run..."
-	$Test=$True
+	$Test = $True
 	$testLog = ".test.log"
 	$testSwtich = "/L"
 }
-else 
-{
+else {
 	"Live run..."
-	$Test=$False
+	$Test = $False
 	$testLog = $Null
 	$testSwtich = $Null 
 } 
@@ -52,50 +48,47 @@ $jobs = Import-Csv $jobFile -Delimiter "|"
 $Global:emailObjArray = @()
 $Global:Subject = "DO - $env:COMPUTERNAME Backup $(date) $((date).dayofweek) - Cooper"
 $Global:Helpdesk = $False
-$ScriptName =  $MyInvocation.MyCommand.Name
-$ScriptLogFile = ".\logs\$ENV:COMPUTERNAME\$ScriptName-$scope-$($(date).year)-$($(date).month)-$($(date).day).log"+$testLog
+$ScriptName = $MyInvocation.MyCommand.Name
+$ScriptLogFile = ".\logs\$ENV:COMPUTERNAME\$ScriptName-$scope-$($(date).year)-$($(date).month)-$($(date).day).log" + $testLog
 $cwd ; CD $cwd
 
 if (!(Test-Path ".\Logs\$ENV:COMPUTERNAME")) { MD ".\Logs\$ENV:COMPUTERNAME" }
-Function Report-Msg ($type = "[INFO]",$msg,$hue = "Green") 
-{
+Function Report-Msg ($type = "[INFO]", $msg, $hue = "Green") {
 	Write-Host "$(date) $type $msg" -ForeGroundColor $hue -BackGroundColor Black
 	Add-Content $ScriptLogFile -Value "$(date) $type $msg"
-	$Global:emailObjArray += New-Object PSObject -Prop @{ Date=$(date); Type=$type; Msg=$msg }
+	$Global:emailObjArray += New-Object PSObject -Prop @{ Date = $(date); Type = $type; Msg = $msg }
 	if ( $type -eq "[ERROR]" ) {
 		"Errors Detected"
 		$Global:Helpdesk = $True
-		if ( $Subject -NotLike "*ERROR*" ) { $Global:Subject = "[ERROR] "+$Subject }
+		if ( $Subject -NotLike "*ERROR*" ) { $Global:Subject = "[ERROR] " + $Subject }
 	}
 }
-Function ErrorCheck 
-{
+Function ErrorCheck {
 	if ( $Error.Count -ne 0 ) { ForEach ($item in $error) { Report-Msg -Type "[ERROR]" -msg "$item" -Hue Red } }
-	else {  Report-Msg -msg "No Powershell errors reported." }
+	else { Report-Msg -msg "No Powershell errors reported." }
 } 
 Report-Msg -Type "[SCRIPT]" -msg "$ENV:COMPUTERNAME | $ENV:USERNAME | $ScriptName $jobFile -Full $Full -Test $Test" -Hue Yellow
-ForEach ( $job in $jobs ) 
-{
+ForEach ( $job in $jobs ) {
 	$excludeFiles = $null
 	$excludeDirs = $null
 	$srcServer = $job.srcServer
 	$srcShare = $job.srcShare
-	$srcSharePath = "\\"+$srcServer+"\"+$srcShare
+	$srcSharePath = "\\" + $srcServer + "\" + $srcShare
 	$dstServer = $job.dstServer
 	$dstShare = $job.dstShare
 	$dstSharePath = "\\$dstServer\$dstShare"
-	$excludeDirs = @("Temp","Adobe Premiere Pro Preview Files","$RECYCLE.BIN","System Volume Information","Autobackup","Updater5","Halo Combat Evolved")
+	$excludeDirs = @("Temp", "Adobe Premiere Pro Preview Files", "$RECYCLE.BIN", "System Volume Information", "Autobackup", "Updater5", "Halo Combat Evolved")
 	if ($job.excludeDirs) { $excludeDirs = $excludeDirs + @($job.excludeDirs.split(",")) }
-	$excludeFiles = $excludeFiles + @("*.log","*desktop.ini","*.db","*.crdownload","*.tmp")
+	$excludeFiles = $excludeFiles + @("*.log", "*desktop.ini", "*.db", "*.crdownload", "*.tmp")
 	if ($job.excludeFiles) { $excludeFiles = $excludeFiles + @($job.excludeFiles.split(",")) }
 	if ( !(Test-Path $srcSharePath) -or !(Test-Path $dstSharePath ) ) {
-		if (!(Test-Path $srcSharePath)) {Report-Msg -type "[ERROR]" -msg "Problem with Src Path: $srcSharePath" -hue Red}
-		if (!(Test-Path $dstSharePath)) {Report-Msg -type "[ERROR]" -msg "Problem with Dst Path: $dstSharePath" -hue Red}
+		if (!(Test-Path $srcSharePath)) { Report-Msg -type "[ERROR]" -msg "Problem with Src Path: $srcSharePath" -hue Red }
+		if (!(Test-Path $dstSharePath)) { Report-Msg -type "[ERROR]" -msg "Problem with Dst Path: $dstSharePath" -hue Red }
 	}
 	else {
 		$dstPath = "\\$dstServer\$dstShare\$scope\$srcShare"
 		if ( !(Test-Path .\logs\$srcServer ) ) { MD .\logs\$srcServer }
-		$RoboLogFile = ".\logs\$srcServer\$srcShare-to-$dstServer-$scope-"+$(date).year+"-"+$(date).month+"-"+$(date).day+".log"+$testLog
+		$RoboLogFile = ".\logs\$srcServer\$srcShare-to-$dstServer-$scope-" + $(date).year + "-" + $(date).month + "-" + $(date).day + ".log" + $testLog
 		if ( (Test-Path $dstpath) -and ($scope -ne "Full") ) {
 			Report-Msg -Type "ACTION" -msg "Removing Prior Daily: $dstpath" -Hue Red
 			if (!$Test) { Get-ChildItem -Path $dstPath -Recurse | Remove-Item -Force -Recurse -Confirm:$False }
@@ -104,9 +97,10 @@ ForEach ( $job in $jobs )
 		}
 		Report-Msg -msg "Src:$srcSharePath | Dst:$dstPath | Options:$options" -Hue Gray
 		# Report-Msg -msg "ROBOCOPY $srcSharePath $dstPath $options /XD $excludeDirs /XF $excludeFiles /LOG:$RoboLogFile /W:0 /R:0 /NFL /NDL /XA:S $testSwtich"
-		ROBOCOPY $srcSharePath $dstPath $options /XD $excludeDirs /XF $excludeFiles /LOG:$RoboLogFile /W:0 /R:0 /NFL /NDL /XA:S $testSwtich
+		# ROBOCOPY $srcSharePath $dstPath $options /XD $excludeDirs /XF $excludeFiles /LOG:$RoboLogFile /W:0 /R:0 /NFL /NDL /XA:S $testSwtich
+		ROBOCOPY $srcSharePath $dstPath $options /XD $excludeDirs /XF $excludeFiles /W:0 /R:0 /NFL /NDL /XA:S $testSwtich
 	}
-	if ($Slow) { Read-Host "ENTER to proceed"}
+	if ($Slow) { Read-Host "ENTER to proceed" }
 }
 ErrorCheck ; $error.clear() # Report any errors and clear $error for next run.
 "Begin Email Call..."
