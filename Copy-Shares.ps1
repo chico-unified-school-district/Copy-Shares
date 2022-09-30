e9a9912e911c0b81858acbbb323040c3dbeb59e4
Justin Cooper <jcooper@chicousd.org>
2022-09-30 07:53:38 -0700

    new

--- Copy-Shares.ps1
+++ Copy-Shares.ps1
@@ -4,17 +4,17 @@
 
 [CmdletBinding()]
 param (
-    $JobFile,
-    [switch]$Mirror,
-    [switch]$WhatIf
+ $JobFile,
+ [switch]$Mirror,
+ [switch]$WhatIf
 )
- 
+
 $jobs = Import-Csv -Path $JobFile -Delimiter '|'
 $excludeFiles = @('*.log', '*desktop.ini', '*.crdownload', '*.tmp', '*.mp4', '*.avi', '*.mpeg', '*.mov', 'thumbs.db', '*.pst', '*.ost')
 if ($WhatIf) { $testSwitch = '/L' }
 foreach ($job in $jobs) {
-    $src = '\\{0}\{1}' -f $job.srcServer, $job.srcShare
-    $dst = '\\{0}\{1}\{2}\{3}' -f $job.dstServer, $job.dstShare, $job.srcServer, $job.srcShare
-    $options = if ($Mirror) { @('/MIR') } else { @('/E', '/M') }
-    ROBOCOPY $src $dst $options /W:0 /R:0 /NFL /NDL /XF $excludeFiles /XA:S $testSwitch
+ $src = '\\{0}\{1}' -f $job.srcServer, $job.srcShare
+ $dst = '\\{0}\{1}\{2}\{3}' -f $job.dstServer, $job.dstShare, $job.srcServer, $job.srcShare
+ $options = if ($Mirror) { @('/MIR') } else { @('/E', '/M') }
+ ROBOCOPY $src $dst $options /W:0 /R:0 /NFL /NDL /XF $excludeFiles /XA:S $testSwitch
 }
--- test/test.ps1
+++
@@ -1,3 +0,0 @@
-foreach ( $item in (import-csv ".\jobs\all-jobs.txt" -Delim "|") ) {
-	$item
-	}
